cebfd9d45f4cbc0141a24c16b41ac572d43e342d
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2016-05-30 22:13:09 -0300

    create basic structure

---
+++ test.lua
@@ -0,0 +1,20 @@
+local array = require 'array'
+local format = string.format
+
+function test(name, func)
+  xpcall(function()
+    func()
+    print(format('[pass] %s', name))
+  end, function(err)
+    print(format('[fail] %s : %s', name, err))  
+  end)
+end
+
+function _equal(a, b)
+  return a == b
+end
+
+function assert_equal(a, b)
+  assert(_equal(a, b))
+end
+
