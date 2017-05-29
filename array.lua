6ab8ed848fbc5d11e68943dd9ab076f8b3d845d2
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-05-29 12:08:13 -0300

    add concat method in array object

--- array.lua
+++ array.lua
@@ -1,144 +1,152 @@
 local function is_table(obj)
-    return type(obj) == 'table'
+  return type(obj) == 'table'
 end
 
 local array = {
-    __VERSION = '1.0.0',
-    __DESCRIPTION = "A small library with useful methods to handle Lua's table when it's working like an Array",
-    __LICENCE = [[
-        The MIT License (MIT)
-        Copyright (c) 2016 Evandro Leopoldino Gonçalves
-        Permission is hereby granted, free of charge, to any person obtaining a copy
-        of this software and associated documentation files (the "Software"), to deal
-        in the Software without restriction, including without limitation the rights
-        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-        copies of the Software, and to permit persons to whom the Software is
-        furnished to do so, subject to the following conditions:
-        The above copyright notice and this permission notice shall be included in all
-        copies or substantial portions of the Software.
-        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-        SOFTWARE.
-    ]]
+  __VERSION = '1.0.0',
+  __DESCRIPTION = "A small library with useful methods to handle Lua's table when it's working like an Array",
+  __LICENCE = [[
+  The MIT License (MIT)
+  Copyright (c) 2016 Evandro Leopoldino Gonçalves
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+  ]]
 }
 
 array = {
-    is_array = function(obj)
-        if not is_table(obj) then return false end
+  is_array = function(obj)
+    if not is_table(obj) then return false end
 
-        local i = 0
-        for _ in pairs(obj) do
-            i = i + 1
-            if obj[i] == nil then return false end
-        end
-
-        return true
-    end,
-
-    is_empty = function(obj)
-        return array.is_array(obj) and #obj == 0
-    end,
-
-    slice = function(obj, start, finish)
-        if array.is_empty(obj) then return {} end
-
-        local output = {}
-        for i=start, finish or #obj do
-            table.insert(output, obj[i])
-        end
-
-        return output
-    end,
-
-    index_of = function(obj, value)
-        for i=1, #obj do
-            if obj[i] == value then
-                return i
-            end
-        end
-
-        return -1
-    end,
-
-    reverse = function(obj)
-        local output = {}
-
-        for i=#obj, 1, -1 do
-            table.insert(output, obj[i])
-        end
-
-        return output
-    end,
-
-    first = function(obj)
-        if not array.is_array(obj) then return nil end
-        return obj[1]
-    end,
-
-    last = function(obj)
-        if not array.is_array(obj) then return nil end
-        return obj[#obj]
-    end,
-
-    max = function(obj)
-        local max = obj[1]
-
-        for i=2, #obj do
-            if obj[i] > max then
-                max = obj[i]
-            end
-        end
-
-        return max
-    end,
-
-    min = function(obj)
-        local min = obj[1]
-
-        for i=2, #obj do
-            if obj[i] < min then
-                min = obj[i]
-            end
-        end
-
-        return min
-    end,
-
-    map = function(obj, callback)
-        local output = {}
-
-        for i=1, #obj do
-            table.insert(output, callback(obj[i], i))
-        end
-
-        return output
-    end,
-
-    filter = function(obj, callback)
-        local output = {}
-
-        for i=1, #obj do
-            if callback(obj[i], i) then
-                table.insert(output, obj[i])
-            end
-        end
-
-        return output
-    end,
-
-    reduce = function(obj, callback, memo)
-        local _memo = memo or 0
-
-        for i=1, #obj do
-            _memo = callback(_memo, obj[i], i)
-        end
-
-        return _memo
+    local i = 0
+    for _ in pairs(obj) do
+      i = i + 1
+      if obj[i] == nil then return false end
     end
+
+    return true
+  end,
+
+  is_empty = function(obj)
+    return array.is_array(obj) and #obj == 0
+  end,
+
+  slice = function(obj, start, finish)
+    if array.is_empty(obj) then return {} end
+
+    local output = {}
+    for i=start, finish or #obj do
+      table.insert(output, obj[i])
+    end
+
+    return output
+  end,
+
+  index_of = function(obj, value)
+    for i=1, #obj do
+      if obj[i] == value then
+        return i
+      end
+    end
+
+    return -1
+  end,
+
+  reverse = function(obj)
+    local output = {}
+
+    for i=#obj, 1, -1 do
+      table.insert(output, obj[i])
+    end
+
+    return output
+  end,
+
+  first = function(obj)
+    if not array.is_array(obj) then return nil end
+    return obj[1]
+  end,
+
+  last = function(obj)
+    if not array.is_array(obj) then return nil end
+    return obj[#obj]
+  end,
+
+  max = function(obj)
+    local max = obj[1]
+
+    for i=2, #obj do
+      if obj[i] > max then
+        max = obj[i]
+      end
+    end
+
+    return max
+  end,
+
+  min = function(obj)
+    local min = obj[1]
+
+    for i=2, #obj do
+      if obj[i] < min then
+        min = obj[i]
+      end
+    end
+
+    return min
+  end,
+
+  map = function(obj, callback)
+    local output = {}
+
+    for i=1, #obj do
+      table.insert(output, callback(obj[i], i))
+    end
+
+    return output
+  end,
+
+  filter = function(obj, callback)
+    local output = {}
+
+    for i=1, #obj do
+      if callback(obj[i], i) then
+        table.insert(output, obj[i])
+      end
+    end
+
+    return output
+  end,
+
+  reduce = function(obj, callback, memo)
+    local _memo = memo or 0
+
+    for i=1, #obj do
+      _memo = callback(_memo, obj[i], i)
+    end
+
+    return _memo
+  end,
+
+  concat = function(obj, obj2)
+    for i=1, #obj2 do
+      table.insert(obj, obj2[i])
+    end
+
+    return obj
+  end
 }
 
 return array
--- test.lua
+++ test.lua
@@ -6,8 +6,8 @@ function test(name, func)
     func()
     print(format('[pass] %s', name))
   end, function(err)
-    print(format('[fail] %s : %s', name, err))  
-  end)
+  print(format('[fail] %s : %s', name, err))
+end)
 end
 
 function _equal(a, b)
@@ -19,140 +19,147 @@ function assert_equal(a, b)
 end
 
 test('is_array should returns false when object is not a table', function()
-    assert_equal(array.is_array('lua'), false)
+  assert_equal(array.is_array('lua'), false)
 end)
 
 test('is_array should returns false when the table is working like a dictionary', function()
-    assert_equal(array.is_array({ language='lua' }), false)
+  assert_equal(array.is_array({ language='lua' }), false)
 end)
 
 test('is_array should returns true when table is empty', function()
-    assert_equal(array.is_array({}), true)
+  assert_equal(array.is_array({}), true)
 end)
 
 test('is_array should returns true when table is working like an array', function()
-    assert_equal(array.is_array({ 'a', 'b', 'c', 'd' }), true)
+  assert_equal(array.is_array({ 'a', 'b', 'c', 'd' }), true)
 end)
 
 test('is_empty should returns false when table has at least one item', function()
-    assert_equal(array.is_empty({ 'a' }), false)
+  assert_equal(array.is_empty({ 'a' }), false)
 end)
 
 test('is_empty should returns false when table does not have any item', function()
-    assert_equal(array.is_empty({}), true)
+  assert_equal(array.is_empty({}), true)
 end)
 
 test('first should returns nil when table is working like a dictionary', function()
-    assert_equal(array.first({language='lua'}), nil)
+  assert_equal(array.first({language='lua'}), nil)
 end)
 
 test('first should returns first item from table', function()
-    assert_equal(array.first({ 'a', 'b', 'c', 'd' }), 'a')
+  assert_equal(array.first({ 'a', 'b', 'c', 'd' }), 'a')
 end)
 
 test('last should returns nil when table is working like a dictionary', function()
-    assert_equal(array.last({language='lua'}), nil)
+  assert_equal(array.last({language='lua'}), nil)
 end)
 
 test('last should returns last item from table', function()
-    assert_equal(array.last({ 'a', 'b', 'c', 'd' }), 'd')
+  assert_equal(array.last({ 'a', 'b', 'c', 'd' }), 'd')
 end)
 
 test('slice should returns a empty table when it does not have any element', function()
-    assert_equal(#array.slice({}, 1, 2), 0)
+  assert_equal(#array.slice({}, 1, 2), 0)
 end)
 
 test('slice should returns a table with values between start index and end index', function()
-    local result = array.slice({ 'lua', 'javascript', 'python', 'ruby', 'c' }, 2, 4)
+local result = array.slice({ 'lua', 'javascript', 'python', 'ruby', 'c' }, 2, 4)
 
-    assert_equal(type(result), 'table')
-    assert_equal(#result, 3)
-    assert_equal(result[1], 'javascript')
-    assert_equal(result[2], 'python')
-    assert_equal(result[3], 'ruby')
+assert_equal(type(result), 'table')
+assert_equal(#result, 3)
+assert_equal(result[1], 'javascript')
+assert_equal(result[2], 'python')
+assert_equal(result[3], 'ruby')
 end)
 
 test('slice should returns a table with every values from start index until last index', function()
-    local result = array.slice({ 'lua', 'javascript', 'python', 'ruby', 'c' }, 2)
+local result = array.slice({ 'lua', 'javascript', 'python', 'ruby', 'c' }, 2)
 
-    assert_equal(type(result), 'table')
-    assert_equal(#result, 4)
-    assert_equal(result[1], 'javascript')
-    assert_equal(result[2], 'python')
-    assert_equal(result[3], 'ruby')
-    assert_equal(result[4], 'c')
+assert_equal(type(result), 'table')
+assert_equal(#result, 4)
+assert_equal(result[1], 'javascript')
+assert_equal(result[2], 'python')
+assert_equal(result[3], 'ruby')
+assert_equal(result[4], 'c')
 end)
 
 test('reverse should returns an inverted table', function()
-    local result = array.reverse({ 'lua', 'javascript', 'python' })
+  local result = array.reverse({ 'lua', 'javascript', 'python' })
 
-    assert_equal(type(result), 'table')
-    assert_equal(#result, 3)
-    assert_equal(result[1], 'python')
-    assert_equal(result[2], 'javascript')
-    assert_equal(result[3], 'lua')
+  assert_equal(type(result), 'table')
+  assert_equal(#result, 3)
+  assert_equal(result[1], 'python')
+  assert_equal(result[2], 'javascript')
+  assert_equal(result[3], 'lua')
 end)
 
 test('map should returns a table with 2, 4, 6 values', function()
-    local result = array.map({ 1, 2, 3 }, function(value)
-        return value * 2
-    end)
+  local result = array.map({ 1, 2, 3 }, function(value)
+    return value * 2
+  end)
 
-    assert_equal(type(result), 'table')
-    assert_equal(#result, 3)
-    assert_equal(result[1], 2)
-    assert_equal(result[2], 4)
-    assert_equal(result[3], 6)
+  assert_equal(type(result), 'table')
+  assert_equal(#result, 3)
+  assert_equal(result[1], 2)
+  assert_equal(result[2], 4)
+  assert_equal(result[3], 6)
 end)
 
 test('filter should returns a table with 10, 15, 20 values', function()
-    local result = array.filter({ 15, 10, 5, 3, 20 }, function(value)
-        return value >= 10
-    end)
+  local result = array.filter({ 15, 10, 5, 3, 20 }, function(value)
+    return value >= 10
+  end)
 
-    assert_equal(type(result), 'table')
-    assert_equal(#result, 3)
-    assert_equal(result[1], 15)
-    assert_equal(result[2], 10)
-    assert_equal(result[3], 20)
+  assert_equal(type(result), 'table')
+  assert_equal(#result, 3)
+  assert_equal(result[1], 15)
+  assert_equal(result[2], 10)
+  assert_equal(result[3], 20)
 end)
 
 test('max should returns the biggest value from a table', function()
-    assert_equal(array.max({ 20, 22, 1, 3, 30, 42 }), 42)
+  assert_equal(array.max({ 20, 22, 1, 3, 30, 42 }), 42)
 end)
 
 test('max should returns nil when table is empty', function()
-    assert_equal(array.max({}), nil)
+  assert_equal(array.max({}), nil)
 end)
 
 test('min should returns the smallest value from a table', function()
-    assert_equal(array.min({ 20, 22, 1, 3, 30, 42 }), 1)
+  assert_equal(array.min({ 20, 22, 1, 3, 30, 42 }), 1)
 end)
 
 test('min should returns nil when table is empty', function()
-    assert_equal(array.min({}), nil)
+  assert_equal(array.min({}), nil)
 end)
 
 test('reduce should returns 90', function()
-    local result = array.reduce({ 20, 30, 40 }, function(memo, value)
-        return memo + value
-    end)
+  local result = array.reduce({ 20, 30, 40 }, function(memo, value)
+    return memo + value
+  end)
 
-    assert_equal(result, 90)
+  assert_equal(result, 90)
 end)
 
 test('reduce should returns 100', function()
-    local result = array.reduce({ 20, 30, 40 }, function(memo, value)
-        return memo + value
-    end, 10)
+  local result = array.reduce({ 20, 30, 40 }, function(memo, value)
+    return memo + value
+  end, 10)
 
-    assert_equal(result, 100)
+  assert_equal(result, 100)
 end)
 
 test('index_of should returns correct position of value in the table', function()
-    assert_equal(array.index_of({ 20, 30, 40, 50 }, 40), 3)
+  assert_equal(array.index_of({ 20, 30, 40, 50 }, 40), 3)
 end)
 
 test('index_of should returns -1 when the value is not in the table', function()
-    assert_equal(array.index_of({ 20, 30, 40 }, 50), -1)
+  assert_equal(array.index_of({ 20, 30, 40 }, 50), -1)
+end)
+
+test('concat should join the two array', function()
+  local result = array.concat({ 1, 2, 3 }, { 4, 5, 6 })
+
+  assert_equal(#result, 6)
+  assert_equal(result[4], 4)
 end)
