7a057a63b2add08abeb1fdd415d1815ddded34c9
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2018-11-26 19:56:26 +0000

    update test.lua

--- test.lua
+++ test.lua
@@ -2,7 +2,7 @@ local array = require 'array'
 local test = require 'simple_test'
 
 test('meta infos', function(a)
-  a.equal(array.__VERSION, '1.2.1')
+  a.equal(array.__VERSION, '1.2.3')
   a.equal(array.__DESCRIPTION,
   "A small library with useful methods to handle Lua's table when it's working like an Array")
 end)
